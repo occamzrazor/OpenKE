609494aa52da39c0fa9882ba7753dba5549d7d82
Daniel Salinas <daniel@occamzrazor.com>
2021-09-15 15:49:36 -0400

    Try making install force build_ext first

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from setuptools import find_namespace_packages, setup, Extension
-from setuptools.command.build_ext import build_ext
+from setuptools import find_namespace_packages, setup, Extension, command
 
 
-class custom_build_ext(build_ext):
+class custom_build_ext(command.build_ext.build_ext):
     def build_extensions(self):
         self.compiler.set_executable("compiler_so", "g++")
         self.compiler.set_executable("compiler_cxx", "g++")
         self.compiler.set_executable("linker_so", "g++")
-        build_ext.build_extensions(self)
+        command.buil_ext.build_ext.build_extensions(self)
 
     def get_ext_filename(self, ext_name):
         return f"{ext_name}.so"
+
+class Build_ext_first(command.install.install):
+    def run(self):
+        self.run_command("build_ext")
+        super(Build_ext_first, self).run()
 
 PACKAGENAME = "OpenKE"
 VERSION = "2021.9.15"
@@ -37,7 +41,10 @@ setup(
         )
     ],
     zip_safe=False,
-    cmdclass={"build_ext": custom_build_ext },
+    cmdclass={
+        'install': Build_ext_first,
+        "build_ext": custom_build_ext,
+    },
     install_requires=INSTALL_REQUIRES,
     python_requires=PYTHON_REQUIRES,
 )
